84c45bec75bf185e4e1aeade6bd33a271e761982
Megan Maldonado <me.maldy@gmail.com>
2018-02-24 13:35:27 -0800

    Update mailer.php

--- includes/mailer.php
+++ includes/mailer.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "me.maldy@gmail.com";
+        $recipient = "youremail@youremail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
